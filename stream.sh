138398061a692151c10a22b8c753e1a9d5278377
LegendYt4k <139982135+LegendYt4k@users.noreply.github.com>
2023-08-02 19:49:34 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -10,5 +10,5 @@ do
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
     -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+    -f flv rtmp://a.rtmp.youtube.com/live2/cd76-kpa4-wj1j-4zmg-8dgx
 done
